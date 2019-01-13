fc964424c6ee3869442b4dfac2944a50b5308e65
Tamozhnii <petek379@ukr.net>
2019-01-13 23:34:54 +0300

    add VerticalLine

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int x, int Up_y, int Down_y, char sym)
+        {
+            for (int y = Up_y; Up_y <= Down_y; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Drow();
+            }
+        }
+    }
+}
